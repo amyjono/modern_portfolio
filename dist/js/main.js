d3dfd1210cdfd2337a4ef3633a229bdbf38f799c
Amy Johnson <amylouise_johnson@hotmail.com>
2019-01-13 14:56:01 +0000

    updates

--- dist/js/main.js
+++ dist/js/main.js
@@ -47,7 +47,6 @@ function check_if_in_view() {
 const menuBtn = document.querySelector('.menu-btn');
 const menu = document.querySelector('.menu');
 const menuNav = document.querySelector('.menu-nav');
-const menuBranding = document.querySelector('.menu-branding');
 const navItems = document.querySelectorAll('.nav-item');
 
 // Set initial state of menu
@@ -60,7 +59,6 @@ function toggleMenu() {
         menuBtn.classList.add('close');
         menu.classList.add('show');
         menuNav.classList.add('show');
-        menuBranding.classList.add('show');
         navItems.forEach(item => item.classList.add('show'));
 
         // Set menu state
@@ -70,7 +68,6 @@ function toggleMenu() {
         menuBtn.classList.remove('close');
         menu.classList.remove('show');
         menuNav.classList.remove('show');
-        menuBranding.classList.remove('show');
         navItems.forEach(item => item.classList.remove('show'));
 
         // Set menu state
