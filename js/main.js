51865a380afc260f0debf61a84bd964ea3db8ccd
Amy Johnson <amylouise_johnson@hotmail.com>
2019-01-13 13:57:00 +0000

    Updates

--- js/main.js
+++ js/main.js
@@ -1,15 +1,45 @@
-//initialize slick slider
+
 $(document).ready(function(){
+
+    //initialize slick slider
     $('.projectSlider').slick({
       autoplay: true,
       dots: true,
       infinite: true,
       speed: 700,
-      autoplaySpeed: 2000,
+      autoplaySpeed: 10000,
       slidesToShow: 1,
       slidesToScroll: 1,
       arrows: false
     });
+
+// Slide up animations
+//Cache reference to window and animation items
+var $animation_elements = $('.animation-element');
+var $window = $(window);
+
+function check_if_in_view() {
+    var window_height = $window.height();
+    var window_top_position = $window.scrollTop();
+    var window_bottom_position = (window_top_position + window_height);
+  
+    $.each($animation_elements, function() {
+      var $element = $(this);
+      var element_height = $element.outerHeight();
+      var element_top_position = $element.offset().top;
+      var element_bottom_position = (element_top_position + element_height);
+  
+      //check to see if this current container is within viewport
+      if ((element_bottom_position >= window_top_position) &&
+        (element_top_position <= window_bottom_position)) {
+        $element.addClass('in-view');
+      }
+    });
+  }
+  
+  $window.on('scroll resize', check_if_in_view);
+  $window.trigger('scroll');
+
   });
 
 
@@ -47,4 +77,3 @@ function toggleMenu() {
         showMenu = false;
     }
 }
-
